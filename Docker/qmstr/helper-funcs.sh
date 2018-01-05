e931e869463a0b0bd9a2404e1cd0b967cc10c340
Markus Herpich <markus@endocode.com>
2018-01-05 12:27:07 +0100

    Add report helper function

--- Docker/qmstr/helper-funcs.sh
+++ Docker/qmstr/helper-funcs.sh
@@ -26,6 +26,10 @@ function printtargets(){
     curl http://localhost:9000/linkedtargets
 }
 
+function report(){
+    curl http://localhost:9000/report?id=$1
+}
+
 function build_cmake(){
     cd /build
     rm -fr build
@@ -38,11 +42,21 @@ function build_cmake(){
 function build_qmstr(){
     PROGS="qmstr-prototype/qmstr/qmstr-master qmstr-prototype/qmstr/qmstr-wrapper"
     for p in $PROGS; do
+        printheader "Installing dependencies for $p"
+        go get -t $p
         printheader "Building $p"
-        go build -i -v $p
-        printheader "Testing $p"
-        go test -i -v $p
+        go build -v $p
         printheader "Installing $p"
         go install -v $p
+    done
+}
+
+function run_unittests() {
+    printheader "Running unit tests"
+    for p in $(ls /qmstr/qmstr); do
+        printheader "Testing package $p"
+        go get -t qmstr-prototype/qmstr/$p
+        go test -i qmstr-prototype/qmstr/$p
+        go test -v qmstr-prototype/qmstr/$p
     done
 }
